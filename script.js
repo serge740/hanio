f765e071b18fa08bb5732eaeb59bf02430e32db5
serge740 <ishimweserge07@gmail.com>
2023-12-15 23:09:23 +0200

    respo

--- script.js
+++ script.js
@@ -9,12 +9,84 @@ var navbar =document.querySelector(".navbar")
 var myBites=document.getElementById("bites");
 var myLong=document.getElementById('long');
 var myShort=document.getElementById('shot');
+var mide=document.querySelector(".mide")
+var li1=document.querySelector(".lia1");
+var man1=document.querySelector(".man1");
 
-myBites.onclick=function(){
+var li2=document.querySelector(".li2");
+var man2=document.querySelector(".man2");
+
+var li3=document.querySelector(".li3");
+var man3=document.querySelector(".man3");
+
+
+var rote1=document.querySelector(".rote1")
+var rote2=document.querySelector(".rote2")
+var rote3=document.querySelector(".rote3")
+var rote4=document.querySelector(".rote4")
+var rote5=document.querySelector(".rote5")
+var rote6=document.querySelector(".rote6")
+
+man1.onclick=function(){
+    
+    li1.classList.toggle("nigga")
+    man1.classList.toggle("ch-man")
+    rote1.classList.toggle("ch-rote")
+    navbar.classList.toggle ("shits")
+    navbar.classList.add("increase")
+   
+}
+man2.onclick=function(){
+    li2.classList.toggle("nigga")
+    man2.classList.toggle("ch-man")
+    rote2.classList.toggle("ch-rote")
+  
+}
+
+man3.onclick=function(){
+    li3.classList.toggle("nigga")
+    man3.classList.toggle("ch-man")
+    rote3.classList.toggle("ch-rote")
+    
+}
+
+var li4=document.querySelector(".li4");
+var man4=document.querySelector(".man4");
+
+var li5=document.querySelector(".li5");
+var man5=document.querySelector(".man5");
+
+var li6=document.querySelector(".li6");
+var man6=document.querySelector(".man6");
+
+man4.onclick=function(){
+    li4.classList.toggle("nigga")
+    man4.classList.toggle("ch-man")
+    rote4.classList.toggle("ch-rote")
+
+
+}
+man5.onclick=function(){
+    li5.classList.toggle("nigga")
+    man5.classList.toggle("ch-man")
+    rote5.classList.toggle("ch-rote")
+
+}
+
+man6.onclick=function(){
+    li6.classList.toggle("nigga")
+    man6.classList.toggle("ch-man")
+    rote6.classList.toggle("ch-rote")
+
+}
+
+function About(){
     myBites.classList.toggle("bit")
     myShort.classList.toggle('shx')
     myLong.classList.toggle('kog')
-    bodY.classList.toggle("hide")
+    mide.classList.toggle("shide")
+     
+     navbar.classList.remove("increase")
      navbar.classList.toggle("shits")
 }
 
@@ -40,3 +112,8 @@ btn.onclick = function(){
     btn.style.cursor="progress"
     
 }
+// document.body.addEventListener("keyup",function(ev){
+//     if(ev.key=="w"){
+//         About()
+//     }
+// })
--- test.js
+++ test.js
@@ -1,9 +1,65 @@
-var myBites=document.getElementById("bites");
-var myLong=document.getElementById('long');
-var myShort=document.getElementById('shot');
+var li1=document.querySelector(".lia1");
+var man1=document.querySelector(".man1");
 
-myBites.onclick=function(){
-    myBites.classList.toggle("bit")
-    myShort.classList.toggle('sho')
-    myLong.classList.toggle('kog')
+var li2=document.querySelector(".li2");
+var man2=document.querySelector(".man2");
+
+var li3=document.querySelector(".li3");
+var man3=document.querySelector(".man3");
+
+
+var rote1=document.querySelector(".rote1")
+var rote2=document.querySelector(".rote2")
+var rote3=document.querySelector(".rote3")
+var rote4=document.querySelector(".rote4")
+var rote5=document.querySelector(".rote5")
+var rote6=document.querySelector(".rote6")
+
+
+man1.onclick=function(){
+    li1.classList.toggle("nigga")
+    man1.classList.toggle("ch-man")
+    rote1.classList.toggle("ch-rote")
+}
+man2.onclick=function(){
+    li2.classList.toggle("nigga")
+    man2.classList.toggle("ch-man")
+    rote2.classList.toggle("ch-rote")
+
+}
+
+man3.onclick=function(){
+    li3.classList.toggle("nigga")
+    man3.classList.toggle("ch-man")
+    rote3.classList.toggle("ch-rote")
+
+}
+
+var li4=document.querySelector(".li4");
+var man4=document.querySelector(".man4");
+
+var li5=document.querySelector(".li5");
+var man5=document.querySelector(".man5");
+
+var li6=document.querySelector(".li6");
+var man6=document.querySelector(".man6");
+
+man4.onclick=function(){
+    li4.classList.toggle("nigga")
+    man4.classList.toggle("ch-man")
+    rote4.classList.toggle("ch-rote")
+
+}
+man5.onclick=function(){
+    li5.classList.toggle("nigga")
+    man5.classList.toggle("ch-man")
+    rote5.classList.toggle("ch-rote")
+
+}
+
+man6.onclick=function(){
+    li6.classList.toggle("nigga")
+    man6.classList.toggle("ch-man")
+    rote6.classList.toggle("ch-rote")
+
 }
